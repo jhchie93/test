fabdfe7f4814a9a749e59048f44ced55f53bc1ec
jh <jh_chie@naver.com>
2021-10-01 12:47:45 +0900

    sql 연습update

--- 01_자료가져오기.sql
+++ 01_자료가져오기.sql
@@ -1,8 +1,61 @@
 SELECT *
 FROM departments;
 
+
+-- 2. 특정 열만 조회하기
 SELECT department_id, department_name
 From departments;
 
 SELECT employee_id, first_name, last_name
 FROM employees;
+
+-- 3. 별칭 붙이기(열의 이름)
+SELECT department_id as 부서번호, department_name 부서이름
+FROM departments;
+
+
+-- 4. 산술 연산자(+,-,*,/)
+SELECT first_name 이름, job_id 직책, salary 월급
+FROM employees;
+-- 월급에 100씩 뺀 열 만들기
+SELECT first_name 이름, job_id 직책, salary 월급, salary-100 월급수정
+FROM employees;
+-- 월급에 10%를 뺀 열 만들기
+SELECT first_name 이름, job_id 직책, salary 월급, salary-salary/10 월급수정
+FROM employees;
+-- 보너스를 주는데 salary에 1000을 뺀 값의 5%로 책정
+SELECT first_name 이름, job_id 직책, salary 월급, (salary-1000)*0.05 보너스
+FROM employees;
+
+-- distinct : 중복 값을 제거
+SELECT DIDISTINCT job_id
+FRFROM employees;
+
+
+--예제1
+SELECT employee_id, first_name, last_name
+FROM employees;
+
+--예제2
+SELECT first_name, salary, salary * 1.1 AS 뉴셀러리
+FROM employees;
+
+--예제3
+SELECT employee_id AS 사원번호 , first_name 이름 , last_name 성
+FROM employees;
+
+--연결 연산자 ||
+SELECT last_name || ' is a ' || job_id AS 직업정보
+FROM employees;
+
+--예제4
+SELECT
+    employee_id,
+    first_name || ' ' || last_name 이름,
+    email || '@company.com' 이메일
+FROM employees;
+
+
+
+
+
---
+++ 02_WHERE절 사용하기.sql
@@ -0,0 +1,119 @@
+--WHERE절
+
+SELECT *
+FROM employees
+WHERE salary > 14000;
+
+SELECT *
+FROM employees
+WHERE last_name = 'King';
+
+SELECT *
+FROM employees
+WHERE hire_date < '02/06/10'; -- 날짜를 비교, 고용일이 2002년 6월 10일 이전
+
+--예제1
+SELECT *
+FROM employees
+WHERE employee_id = 100;
+
+--예제2
+
+SELECT *
+FROM employees
+WHERE first_name = 'David';
+
+--예제3
+
+SELECT *
+FROM employees
+WHERE employee_id <= 105;
+
+--예제4
+
+SELECT *
+FROM job_history
+WHERE start_date > '06/03/03';
+
+--예제5
+SELECT *
+FROM departments
+WHERE location_id != 1700;
+
+-- 논리 연산자 AND OR NOT
+
+SELECT last_name, department_id, salary
+FROM employees
+WHERE department_id = 60 OR (department_id = 80 AND salary > 10000);
+
+SELECT *
+FROM employees
+WHERE NOT ( hire_date > '04/01/01' or salary > 5000);
+
+--예제1
+SELECT *
+FROM employees
+WHERE salary > 4000 AND job_id = 'IT_PROG';
+
+SELECT *
+FROM employees
+WHERE salary > 4000 AND (job_id = 'IT_PROG' OR job_id = 'FI_ACCOUNT');
+
+--IN 연산자 ()안의 값과 같을 때 OR 
+SELECT *
+FROM employees
+WHERE salary IN (4000, 3000, 2700);
+
+--예제1
+SELECT *
+FROM employees
+WHERE salary IN (10000, 17000, 24000);
+
+SELECT *
+FROM employees
+WHERE department_id NOT IN (30, 50, 80, 100, 110);
+
+--BETWEEN
+
+SELECT *
+FROM employees
+WHERE salary BETWEEN 9000 AND 10000;
+
+--예제1
+
+SELECT *
+FROM employees
+WHERE salary BETWEEN 10000 AND 20000;
+
+--예제2
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '04/01/01' AND '04/12/30';
+
+--예제3
+SELECT *
+FROM employees
+WHERE salary NOT BETWEEN 7000 AND 17000;
+
+--LIKE 연산자
+
+SELECT * FROM employees WHERE last_name LIKE 'B%'; --대문자 B로 시작, 뒤는 상관없음.
+SELECT * FROM employees WHERE last_name LIKE '%b%'; --문자열 중간에 b가 있으면 전부 출력
+SELECT * FROM employees WHERE last_name LIKE '____y'; --언더바는 정확한 자리수를 표현
+
+--예제1
+SELECT * FROM employees WHERE job_id LIKE '%AD%';
+--예제2
+SELECT * FROM employees WHERE job_id Like '%AD___';
+--예제3
+SELECT * FROM employees WHERE phone_number LIKE '%1234';
+--예제4
+SELECT * FROM employees WHERE phone_number NOT LIKE '%3%' AND phone_number LIKE '%9';
+--예제5
+SELECT * FROM employees WHERE job_id LIKE '%MGR%' OR job_id LIKE '%ASST%';
+
+
+--IS NULL 연산자
+SELECT * FROM employees WHERE commission_pct IS NULL;
+SELECT * FROM employees WHERE commission_pct IS NOT NULL;
+
